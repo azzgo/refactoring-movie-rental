fa8f5594bc876ae03321ecebc65bf40957e1c9e4
Yishan Wen <wittywys@gmail.com>
2021-04-06 16:36:40 +0800

    refactor without change test

--- src/main/java/cn/xpbootcamp/refactor/Customer.kt
+++ src/main/java/cn/xpbootcamp/refactor/Customer.kt
@@ -1,49 +1,40 @@
 package cn.xpbootcamp.refactor
 
-import java.lang.StringBuilder
 import java.util.*
+import kotlin.text.StringBuilder
 
 class Customer internal constructor(val name: String) {
     private val rentals = Vector<Rental>()
+
     fun addRental(rental: Rental) {
         rentals.addElement(rental)
     }
 
     fun statement(): String {
-        var totalAmount = 0.0
-        var frequentRenterPoints = 0
-        val rentals = rentals.elements()
-        val result = StringBuilder("Rental Record for $name：\n")
-        while (rentals.hasMoreElements()) {
-            val each = rentals.nextElement()
-            //show figures for this rental
-            //determine amounts for each line
-            var thisAmount = 0.0
-            when (each.movie.priceCode) {
-                Movie.HISTORY -> {
-                    thisAmount += 2.0
-                    if (each.daysRented > 2) thisAmount += (each.daysRented - 2) * 1.5
-                }
-                Movie.NEW_RELEASE -> thisAmount += (each.daysRented * 3).toDouble()
-                Movie.CAMPUS -> {
-                    thisAmount += 1.5
-                    if (each.daysRented > 3) thisAmount += (each.daysRented - 3) * 1.5
-                }
-            }
-            //add frequent renter points
-            frequentRenterPoints++
-            if (each.movie.priceCode == Movie.NEW_RELEASE && each.daysRented > 1) frequentRenterPoints++
+        val result = StringBuilder(getNameStatement())
 
-            //show figures for this rental
-            result.append("\t")
-                .append(each.movie.title)
-                .append("\t")
-                .append(thisAmount).append("\n")
-            totalAmount += thisAmount
-        }
-        //add footer lines
-        result.append("Amount owed is ").append(totalAmount).append("\n")
-        result.append("You earned ").append(frequentRenterPoints).append(" frequent renter points")
+        result.append(getRentalStatement())
+        result.append(getTotalAmountStatement())
+        result.append(getFrequentRenterPointsStatement())
         return result.toString()
+    }
+
+    private fun getNameStatement() = "Rental Record for $name：\n"
+
+    private fun getRentalStatement(): String {
+        return rentals.map() {
+            "\t${it.movie.title}\t${it.calculateThisAmount()}\n"
+        }.joinToString("")
+    }
+
+
+    private fun getTotalAmountStatement(): String {
+        val totalAmount = rentals.sumByDouble { it.calculateThisAmount() }
+        return "Amount owed is $totalAmount\n"
+    }
+
+    private fun getFrequentRenterPointsStatement(): String {
+        val frequentRenterPoints = rentals.sumBy { it.calculateFrequentRenterPoints() }
+        return "You earned $frequentRenterPoints frequent renter points"
     }
 }
--- src/main/java/cn/xpbootcamp/refactor/Rental.kt
+++ src/main/java/cn/xpbootcamp/refactor/Rental.kt
@@ -1,3 +1,24 @@
 package cn.xpbootcamp.refactor
 
-class Rental internal constructor(val movie: Movie, val daysRented: Int)
+data class Rental internal constructor(val movie: Movie, val daysRented: Int) {
+    fun calculateThisAmount(): Double {
+        var thisAmount = 0.0
+        when (movie.priceCode) {
+            Movie.HISTORY -> {
+                thisAmount += 2.0
+                if (daysRented > 2) thisAmount += (daysRented - 2) * 1.5
+            }
+            Movie.NEW_RELEASE -> thisAmount += (daysRented * 3).toDouble()
+            Movie.CAMPUS -> {
+                thisAmount += 1.5
+                if (daysRented > 3) thisAmount += (daysRented - 3) * 1.5
+            }
+        }
+
+        return thisAmount
+    }
+
+    fun calculateFrequentRenterPoints(): Int {
+        return if (movie.priceCode == Movie.NEW_RELEASE && daysRented > 1) 2 else 1
+    }
+}
